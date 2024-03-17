d6ec5181b76bcce51da0c3893ce9bc8ac9e4acf8
Nate Laffan <nate.laffan@gmail.com>
2024-03-16 22:19:37 -0700

    Added support for mobile and a dragging demo

--- src/main.js
+++ src/main.js
@@ -1,20 +1,28 @@
 // src/main.js
 import Phaser from 'phaser';
+import isMobile from './helpers/isMobile';
 import { LoadScene } from './scenes/Load';
 import { PlayScene } from './scenes/Play';
 
-document.getElementById('loading').style.display = 'none';
+const gameParent = "game";
 
-const gameConfig  = {
-  parent: "game",
+// Remove "loading" string
+document.getElementById('loading').style.display = 'none';
+// Check to see if on a mobile browser
+const onMobile = isMobile();
+// Add border radius if you are not
+document.getElementById(gameParent).style.borderRadius = onMobile ? "0" : "20px";
+
+
+const gameConfig = {
+  parent: gameParent,
   type: Phaser.AUTO,
   backgroundColor: "#cfcfcf",
-  height: 600,
-  width: 600,
+  width: onMobile ? window.innerWidth: 600,
+  height: onMobile ? window.innerHeight: 600,
   physics: {
     default: "arcade",
     arcade: {
-      // debug: true,
       gravity: {
         x: 0,
         y: 0,
--- src/scenes/Play.js
+++ src/scenes/Play.js
@@ -1,4 +1,4 @@
-import { Scene, GameObjects } from "phaser";
+import { Scene } from "phaser";
 
 export class PlayScene extends Scene {
   constructor() {
@@ -7,22 +7,32 @@ export class PlayScene extends Scene {
 
   preload() {}
 
-  create() {
+  create() { 
     this.add.text(50, 50, "Phaser 3.7", {
       fontFamily: "digitalDisco",
       fontSize: "32px",
       fill: "#ffffff",
     });
 
-    this.demoImg = this.add.sprite(
-        50,
-        90,
-        "demo_image"
-      );
-      this.demoImg.setOrigin(0);
-      this.demoImg.setAlpha(0.1);
-      this.demoImg.setScale(0.5);
+ // Add the sprite and make it interactive
+    this.demoImg = this.add.sprite(50, 90, "demo_image").setInteractive({ useHandCursor: true });
 
+    // Set initial properties
+    this.demoImg.setOrigin(0);
+    this.demoImg.setAlpha(0.1);
+    this.demoImg.setScale(0.5);
+
+    this.input.setDraggable(this.demoImg);
+    this.input.on('dragstart', function (pointer, gameObject) {
+      gameObject.setAlpha(0.5);
+    });
+    this.input.on('drag', function (pointer, gameObject, dragX, dragY) {
+      gameObject.x = dragX;
+      gameObject.y = dragY;
+    });
+    this.input.on('dragend', function (pointer, gameObject) {
+      gameObject.setAlpha(0.1);
+    });
   }
 
   update() {}
